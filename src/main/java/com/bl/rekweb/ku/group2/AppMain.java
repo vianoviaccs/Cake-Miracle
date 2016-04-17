de37f327856731400dd567473dfce36ca0fe8400
andry <andry.sunandar@gmail.com>
2016-04-17 21:28:45 +0700

    update license

--- src/main/java/com/bl/rekweb/ku/group2/AppMain.java
+++ src/main/java/com/bl/rekweb/ku/group2/AppMain.java
@@ -1,14 +1,14 @@
 /**
- * Innovez-One, Proprietary Software Cloud Communications
- *  Copyright (c) 2015, Innovez-One and individual contributors
+ * Budi Luhur University, Proprietary Software Cloud Communications
+ *  Copyright (c) 2016, budiluhur.ac.id and individual contributors
  *  by the @authors tag.
  *
  *  This program is Proprietary Software: you can not redistribute it and/or modify
- *  without license from Innovez-One.
+ *  without license from Budi Luhur University.
  *
- *  Website : http://www.innovez-one.com/
- *  Report bugs to <techsupport@innovez-one.com>.
- *  Copyright (C) 2015 PT. Innovez-One. All rights reserved.
+ *  Website : http://www.budiluhur.ac.id/
+ *  Report bugs to <techsupport@budiluhur.ac.id>.
+ *  Copyright (C) 2016 Budi Luhur University. All rights reserved.
  */
 package com.bl.rekweb.ku.group2;
 
