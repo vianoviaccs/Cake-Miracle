0804c8e05fb4311cfe987abcfbf20935f5d2b3bb
andry <andry.sunandar@gmail.com>
2016-04-17 21:23:26 +0700

    add AppMain Class

---
+++ src/main/java/com/bl/rekweb/ku/group2/AppMain.java
@@ -0,0 +1,19 @@
+/**
+ * Innovez-One, Proprietary Software Cloud Communications
+ *  Copyright (c) 2015, Innovez-One and individual contributors
+ *  by the @authors tag.
+ *
+ *  This program is Proprietary Software: you can not redistribute it and/or modify
+ *  without license from Innovez-One.
+ *
+ *  Website : http://www.innovez-one.com/
+ *  Report bugs to <techsupport@innovez-one.com>.
+ *  Copyright (C) 2015 PT. Innovez-One. All rights reserved.
+ */
+package com.bl.rekweb.ku.group2;
+
+/**
+ * Author andry on 17/04/16.
+ */
+public class AppMain {
+}
