ca4b00db7408320419adf0142074a88e1231eb5b
andry <andry.sunandar@gmail.com>
2016-04-19 03:24:29 +0700

    add cake model

---
+++ src/main/java/com/bl/rekweb/ku/group2/model/Cake.java
@@ -0,0 +1,19 @@
+/**
+ * Budi Luhur University, Proprietary Software Cloud Communications
+ *  Copyright (c) 2016, budiluhur.ac.id and individual contributors
+ *  by the @authors tag.
+ *
+ *  This program is Proprietary Software: you can not redistribute it and/or modify
+ *  without license from Budi Luhur University.
+ *
+ *  Website : http://www.budiluhur.ac.id/
+ *  Report bugs to <techsupport@budiluhur.ac.id>.
+ *  Copyright (C) 2016 Budi Luhur University. All rights reserved.
+ */
+package com.bl.rekweb.ku.group2.model;
+
+/**
+ * Author andry on 19/04/16.
+ */
+public class Cake {
+}
